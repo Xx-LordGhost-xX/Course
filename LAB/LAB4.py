55e7c95b0b75a43581d26b1023b1ca4028d839cf
LordGhost <69468739+Xx-LordGhost-xX@users.noreply.github.com>
2020-08-12 07:49:05 +0300

    Delete LAB4.py

--- LAB/LAB4.py
+++
@@ -1,8 +0,0 @@
-dict_names = {"ghost": 25000, "adi": 30000, "yakir": 76000, "ortal": 66000, "gal": 120000}
-print(dict_names)
-summay = dict_names["ghost"] + dict_names["gal"]
-print("The summary is: " + str(summay))
-dict_names.update({"yoel": summay})
-print(dict_names)
-print("Number of entries: " + str(len(dict_names)))
-print("adi" in dict_names)
