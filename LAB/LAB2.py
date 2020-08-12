7fb897c343d839db5a263700f581c381c5995c65
LordGhost <69468739+Xx-LordGhost-xX@users.noreply.github.com>
2020-08-12 07:48:51 +0300

    Delete LAB2.py

--- LAB/LAB2.py
+++
@@ -1,6 +0,0 @@
-name = "ghost"
-age = 40
-mail = "ghost@gmail.com"
-print("Full name: " + name + "\nage: " + str(age) + "\nmail: " + mail)
-print("\n\nFull name: " + name[::-1] + "\nage: " + str(age * 3))
-print("elia" in "ghost adi yakir anna shimon yael gal adam shahar yana")
