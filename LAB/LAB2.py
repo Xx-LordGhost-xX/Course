19a3ccf1a3a2a49ce05595340d9abe26db8cd0d0
Asaf <asafyarimi5@gmail.com>
2020-08-12 07:55:08 +0300

    Course

---
+++ LAB/LAB2.py
@@ -0,0 +1,6 @@
+name = "ghost"
+age = 40
+mail = "ghost@gmail.com"
+print("Full name: " + name + "\nage: " + str(age) + "\nmail: " + mail)
+print("\n\nFull name: " + name[::-1] + "\nage: " + str(age * 3))
+print("elia" in "ghost adi yakir anna shimon yael gal adam shahar yana")
