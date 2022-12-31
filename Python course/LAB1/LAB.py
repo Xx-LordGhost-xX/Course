8f8695dbbcdc237f12b4a565f0ed57e19f7bbe44
LordGhost <69468739+Xx-LordGhost-xX@users.noreply.github.com>
2023-01-01 00:37:16 +0200

    Delete LAB.py

--- Python course/LAB1/LAB.py
+++
@@ -1,4 +0,0 @@
-num = int(input("Enter a number with 4 digits: "))
-
-print("Alafim=" + str(num // 1000) + "\nMeot=" + str((num % 1000) // 100) + "\nAsarot=" + str(
-    ((num % 100) // 10)) + "\nAhadot=" + str((num % 10)))
